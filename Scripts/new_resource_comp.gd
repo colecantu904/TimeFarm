306c3c887c34a95627c0e0357e51c75aaa38b6d0
Prototyype <93347830+Prototyype@users.noreply.github.com>
2024-05-20 19:38:01 -0500

    fixed icon anim

--- Scripts/new_resource_comp.gd
+++ Scripts/new_resource_comp.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 @export var global_timer : Node
-@export var tile_to_edit : TileMap
 @export var area : Area2D
 # in runtime, the source will be passed by the player
 @export var resource : Source =Corn.new()
@@ -18,9 +17,8 @@ func _ready():
 	# sets the resources values
 	resource.create()
 
-	
-	tile_to_edit.add_layer(1)
-	tile_to_edit.set_cell(1, Vector2i(-2,-1), 0, Vector2i(resource.icon_x_atlas, resource.icon_y_atlas))
+	icon.add_layer(1)
+	icon.set_cell(1, Vector2i(0,-1), 0, Vector2i(resource.icon_x_atlas, resource.icon_y_atlas))
 	
 	# set the gloabl time started
 	time_started = global_timer.timepassed
@@ -43,7 +41,7 @@ func _process(delta):
 				# ui element for collection crops
 				if Input.is_action_just_pressed("click"):
 					time_started = global_timer.timepassed
-					tile_to_edit.remove_layer(1)
+					icon.remove_layer(1)
 					resource = null
 					anim.play("empty")
 					print("collected!")
