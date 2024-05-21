1aeb901cba47f956e644096a8f3592c78d964c09
Prototyype <93347830+Prototyype@users.noreply.github.com>
2024-05-20 21:55:45 -0500

    y sort and icons

---
+++ Scripts/CharacterBody2D.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+var current_animation = "idle"
+@export var speed = 300
+var angle = 0
+func _physics_process(delta):
+	current_animation = "idle"
+
+	var mouse = get_local_mouse_position()
+	angle = snappedf(mouse.angle(), PI/4) / (PI/4)
+	angle = wrapi(int(angle), 0, 8)
+
+	if Input.is_action_pressed("click") and mouse.length() > 10:
+		current_animation = "run"
+		velocity = mouse.normalized() * speed
+		move_and_slide()
+	$AnimatedSprite2D.animation = current_animation + str(angle)
